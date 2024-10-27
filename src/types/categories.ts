f36c715616bc8e27d3d99160693dff6dd255c9a3
Ivan Wong <ivanwongrubblishbin@gmail.com>
2024-10-27 22:08:33 +0800

    chore: dynamically added categories type

--- src/types/categories.ts
+++ src/types/categories.ts
@@ -1,11 +1,18 @@
+import bloglist from "../pages/bloglist.json";
+
+// Extract unique categories from the bloglist JSON data
+const dynamicCategories = Array.from(
+  new Set(bloglist.bloglist.map((post) => post.category))
+);
+
 export const categories = [
   "Software Engineering",
   "Electronic Engineering",
   "Website Development",
   "Theory and Principle",
   "SEO",
-  "Network",
   "Others",
+  ...dynamicCategories, // Add dynamic categories here
 ] as const;
 
 export type Category = (typeof categories)[number];
